692d81a0d375ad810efc3a67f758dba1155d9765
frose <937092196@qq.com>
2022-02-12 23:24:27 +0800

    测试demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>你好 World</div>
 </template>
 
 <script lang="ts">
